8d71cd4a4607f9aa1b6766df4fd6b85210c0c8e1
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-01 20:24:32 +0400

    Smoothing the Animation

--- timer.js
+++ timer.js
@@ -1,41 +1,51 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-      this.durationInput = durationInput;
-      this.startButton = startButton;
-      this.pauseButton = pauseButton;
-      if (callbacks) {
-        this.onStart = callbacks.onStart;
-      }
-  
-      this.startButton.addEventListener('click', this.start);
-      this.pauseButton.addEventListener('click', this.pause);
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
-    start = () => {
-        if (this.onStart) {
-          this.onStart();
-        }
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-      };
-    
-      pause = () => {
-        clearInterval(this.interval);
-      };
-    
-      tick = () => {
-        if (this.timeRemaining <= 0) {
-          this.pause();
-        } else {
-          this.timeRemaining = this.timeRemaining - 1;
-        }
-      };
-    
-      get timeRemaining() {
-        return parseFloat(this.durationInput.value);
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 50);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
       }
-    
-      set timeRemaining(time) {
-        this.durationInput.value = time;
+    } else {
+      this.timeRemaining = this.timeRemaining - .05;
+      if (this.onTick) {
+        this.onTick();
       }
     }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
+
     
